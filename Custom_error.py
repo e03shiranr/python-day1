00b9b0179888a2e268acd5015b908e6f664338cb
e03shiranr <e03shiranr@gmail.com>
2025-06-20 14:39:51 +0530

    7th commit

---
+++ Custom_error.py
@@ -0,0 +1,24 @@
+class NegativeNumberError(ValueError):
+    pass
+
+def get_positive_number():
+    while True:
+        try:
+            num_str = input("Please neter a positive number :")
+            number = float(num_str)
+
+            if number < 0:
+                raise NegativeNumberError("Number must be positive.")
+            return number
+        except NegativeNumberError as e:
+            print(f"Error {e}")
+
+if __name__ == "__main__":
+    try:
+        positive_num = get_positive_number()
+        print(f"You entered a positive number: {positive_num}")
+    except NegativeNumberError as e:
+        print(f"Caught and error in the main scirpt: {e}")
+    except Exception as e:
+        print(f"unexpected eroor occured: {e}")
+        
