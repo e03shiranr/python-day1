f88c8114e57f77974ed7c189ab45daba844f89f7
e03shiranr <e03shiranr@gmail.com>
2025-06-25 09:53:46 +0530

    2025-06-25_3

--- set.py
+++ set.py
@@ -1,6 +1,6 @@
 print("--- Set Operation ---")
-set_a = {10, 20, 30, 40, 50}
-set_b = {40, 50, 60, 70, 80}
+set_a = {10, 20, 30, 40, 50, 60, 70}
+set_b = {40, 50, 60, 70, 80, 90, 100}
 
 print(f"Set A: {set_a}")
 print(f"Set B: {set_b}")
@@ -10,3 +10,16 @@ print(f"Union of set_a and set_b is: {union_set}")
 
 intersection_set = set_a.intersection(set_b)
 print(f"Intersection of set_a and set_b is: {intersection_set}")
+
+difference_set = set_a.difference(set_b)
+print(f"Differece (set a - set b): {difference_set}")
+print("\n -- List De-duplication using Sets ---")
+
+my_list = [1, 2, 2, 3, 4, 4, 5, 1, 6]
+print(f"Original list with duplicates: {my_list}")
+
+unique_elements_set = set(my_list)
+print(f"Set created from list is: {unique_elements_set}")
+
+unique_set = list(unique_elements_set)
+print(f"Unique Set: {unique_set}")
